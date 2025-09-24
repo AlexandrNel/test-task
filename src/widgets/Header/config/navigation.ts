0a7caab76cd397cdc19a309d382185f1683b9f77
Alexandr Nelyubov <sashanelyubov2005@gmail.com>
2025-09-25 00:28:58 +0300

    feat: добавлен компонент Header с навигацией

---
+++ src/widgets/Header/config/navigation.ts
@@ -0,0 +1,18 @@
+export const navigation = [
+  {
+    to: "/posts",
+    title: "Посты",
+  },
+  {
+    to: "/users",
+    title: "Пользователи",
+  },
+  {
+    to: "/todo",
+    title: "Задачи",
+  },
+  {
+    to: "/photos",
+    title: "Фото",
+  },
+];
