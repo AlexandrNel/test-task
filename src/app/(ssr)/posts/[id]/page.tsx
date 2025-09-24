fecaac4532376d9ef1bc0f5a6639659987b34a8e
Alexandr Nelyubov <sashanelyubov2005@gmail.com>
2025-09-25 00:46:55 +0300

    feat(SSR): добавлена страница для отображения поста с динамической генерацией метаданных

---
+++ src/app/(ssr)/posts/[id]/page.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { getPostById } from "@/entities/post/";
+import { capitalizeFirstLetter } from "@/shared/lib/utils/capitalizeFirstLetter";
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const id = (await params).id;
+  const post = await getPostById(id);
+  return {
+    title: capitalizeFirstLetter(post.title),
+    description: capitalizeFirstLetter(post.body),
+  };
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
+  const post = await getPostById(id);
+  return (
+    <div className="container">
+      <h1 className="text-lg font-bold">{capitalizeFirstLetter(post.title)}</h1>
+      <p>{capitalizeFirstLetter(post.body)}</p>
+    </div>
+  );
+}
