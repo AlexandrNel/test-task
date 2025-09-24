f1daef3f3bfe45cb2cfa86caa3cbc53a24217e69
Alexandr Nelyubov <sashanelyubov2005@gmail.com>
2025-09-25 00:47:07 +0300

    feat: обновлен компонент Loading для отображения нескольких элементов загрузки

---
+++ src/app/(ssr)/posts/loading.tsx
@@ -0,0 +1,16 @@
+export default function Loading() {
+  return (
+    <div className="container">
+      <div className="flex flex-col pt-4">
+        <div className="animate-pulse">
+          {[...Array(5)].map((_, i) => (
+            <div
+              key={`loading-${i}`}
+              className="h-25 bg-gray-200 rounded mb-2"
+            ></div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
