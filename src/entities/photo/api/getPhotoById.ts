07d698c2958dd8fb4ea8c4994ff5f9dd090f0aad
Alexandr Nelyubov <sashanelyubov2005@gmail.com>
2025-09-29 23:46:51 +0300

    fix

--- src/entities/photo/api/getPhotoById.ts
+++ src/entities/photo/api/getPhotoById.ts
@@ -1,9 +1,11 @@
 import type { PhotoDto } from "../types";
 import { PUBLIC_PHOTO_API_URL } from "@/shared/lib/consts";
 import { mapPhotoDtoToPhoto } from "../utils";
+import { notFound } from "next/navigation";
 
 export async function getPhotoById(id: string) {
   const res = await fetch(`${PUBLIC_PHOTO_API_URL}/${id}`);
   const data: PhotoDto = await res.json();
+  if (!data.photo) notFound();
   return mapPhotoDtoToPhoto(data);
 }
